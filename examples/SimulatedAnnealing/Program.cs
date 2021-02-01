0c58b88bbf3147f341d27715deadb6c7d3365501
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-01 23:15:31 +0900

    fix

--- examples/SimulatedAnnealing/Program.cs
+++ examples/SimulatedAnnealing/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 
+using OpenJijDotNet;
+
 namespace SimulatedAnnealing
 {
     internal class Program
     {
 
+        #region Methods
+
         private static void Main(string[] args)
         {
         }
+
+        #region Helpers
+        
+        private static void ShowSpins(Spins spins)
+        {
+            foreach (var s in spins)
+                Console.Write($"{s} ");
+            Console.WriteLine();
+        }
+        
+        #endregion
+
+        #endregion
         
     }
-    
+
 }
--- examples/SimulatedQuantumAnnealing/Program.cs
+++ examples/SimulatedQuantumAnnealing/Program.cs
@@ -5,10 +5,25 @@ namespace SimulatedQuantumAnnealing
     internal class Program
     {
 
+        #region Methods
+
         private static void Main(string[] args)
         {
         }
+
+        #region Helpers
         
+        private static void ShowSpins(Spins spins)
+        {
+            foreach (var s in spins)
+                Console.Write($"{s} ");
+            Console.WriteLine();
+        }
+        
+        #endregion
+
+        #endregion
+
     }
 
 }
