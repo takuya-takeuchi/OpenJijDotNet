f510abee6a4dad230dadfdd1878c7362d1edd774
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-05 01:31:21 +0900

    fix

--- test/OpenJijDotNet.Tests/Graph/DenseTest.cs
+++ test/OpenJijDotNet.Tests/Graph/DenseTest.cs
@@ -17,6 +17,45 @@ namespace OpenJijDotNet.Tests.Graphs
             this.DisposeAndCheckDisposedState(dense);
         }
 
+        [Fact]
+        public void Spins()
+        {
+            const uint N = 124;
+            var dense = new Dense<double>(124);
+            Assert.Equal(dense.Spins, N);
+            this.DisposeAndCheckDisposedState(dense);
+        }
+
+        [Fact]
+        public void J()
+        {
+            const uint N = 10;
+            var dense = new Dense<double>(124);
+
+            for (uint i = 0; i < N; i++)
+                for (uint j = 0; j < N; j++)
+                    dense.J[i, j] = (i == j) ? 0 : -1;
+            for (uint i = 0; i < N; i++)
+                for (uint j = 0; j < N; j++)
+                    Assert.Equal(dense.J[i, j], (i == j) ? 0 : -1);
+            
+            this.DisposeAndCheckDisposedState(dense);
+        }
+
+        [Fact]
+        public void H()
+        {
+            const uint N = 10;
+            var dense = new Dense<double>(124);
+
+            for (uint i = 0; i < N; i++)
+                dense.H[i] = -1;
+            for (uint i = 0; i < N; i++)
+                Assert.Equal(dense.H[i], -1);
+            
+            this.DisposeAndCheckDisposedState(dense);
+        }
+
     }
 
 }
--- test/OpenJijDotNet.Tests/Graph/SparseTest.cs
+++ test/OpenJijDotNet.Tests/Graph/SparseTest.cs
@@ -13,8 +13,47 @@ namespace OpenJijDotNet.Tests.Graphs
         [Fact]
         public void Create()
         {
-            var dense = new Sparse<double>(500);
-            this.DisposeAndCheckDisposedState(dense);
+            var sparse = new Sparse<double>(500);
+            this.DisposeAndCheckDisposedState(sparse);
+        }
+
+        [Fact]
+        public void Spins()
+        {
+            const uint N = 124;
+            var sparse = new Sparse<double>(124);
+            Assert.Equal(sparse.Spins, N);
+            this.DisposeAndCheckDisposedState(sparse);
+        }
+
+        [Fact]
+        public void J()
+        {
+            const uint N = 10;
+            var sparse = new Sparse<double>(124);
+
+            for (uint i = 0; i < N; i++)
+                for (uint j = 0; j < N; j++)
+                    sparse.J[i, j] = (i == j) ? 0 : -1;
+            for (uint i = 0; i < N; i++)
+                for (uint j = 0; j < N; j++)
+                    Assert.Equal(sparse.J[i, j], (i == j) ? 0 : -1);
+            
+            this.DisposeAndCheckDisposedState(sparse);
+        }
+
+        [Fact]
+        public void H()
+        {
+            const uint N = 10;
+            var sparse = new Sparse<double>(124);
+
+            for (uint i = 0; i < N; i++)
+                sparse.H[i] = -1;
+            for (uint i = 0; i < N; i++)
+                Assert.Equal(sparse.H[i], -1);
+            
+            this.DisposeAndCheckDisposedState(sparse);
         }
 
     }
