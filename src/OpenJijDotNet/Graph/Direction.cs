e7628cffc8d82380af4382860c1858a584db1cd0
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-06 23:56:49 +0900

    feat: add enum

---
+++ src/OpenJijDotNet/Graph/Direction.cs
@@ -0,0 +1,18 @@
+// ReSharper disable once CheckNamespace
+namespace OpenJijDotNet.Graphs
+{
+
+    public enum ChimeraDirection 
+    {
+
+        PlusRow,
+
+        MinusRow,
+
+        PlusColumn,
+
+        MinusColumn
+
+    }
+
+}
