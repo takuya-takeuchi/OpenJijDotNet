a8c6ba2fb72f09b8091931b1a517d46fae6273a2
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-10 21:57:54 +0900

    feat: implement manage

---
+++ src/OpenJijDotNet/PInvoke/Utility/ClassicalUpdaterParameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using uint8_t = System.Byte;
+using uint16_t = System.UInt16;
+using uint32_t = System.UInt32;
+using int64_t = System.Int64;
+using int8_t = System.SByte;
+using int16_t = System.Int16;
+using int32_t = System.Int32;
+
+// ReSharper disable once CheckNamespace
+namespace OpenJijDotNet
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern IntPtr utility_ClassicalUpdaterParameter_new(double beta);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern void utility_ClassicalUpdaterParameter_delete(IntPtr parameter);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern double utility_ClassicalUpdaterParameter_get_tuple(IntPtr parameter);
+
+    }
+
+}
---
+++ src/OpenJijDotNet/PInvoke/Utility/TransverseFieldUpdaterParameter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using uint8_t = System.Byte;
+using uint16_t = System.UInt16;
+using uint32_t = System.UInt32;
+using int64_t = System.Int64;
+using int8_t = System.SByte;
+using int16_t = System.Int16;
+using int32_t = System.Int32;
+
+// ReSharper disable once CheckNamespace
+namespace OpenJijDotNet
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern IntPtr utility_TransverseFieldUpdaterParameter_new(double beta,
+                                                                                double s);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern void utility_TransverseFieldUpdaterParameter_delete(IntPtr parameter);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern void utility_TransverseFieldUpdaterParameter_get_tuple(IntPtr parameter,
+                                                                                    out double item1,
+                                                                                    out double item2);
+
+    }
+
+}
