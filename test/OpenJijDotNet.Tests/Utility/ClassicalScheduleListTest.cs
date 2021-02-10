7200cbf0f8c331f315a467aa51a243d3ddc40747
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-10 22:02:11 +0900

    fix: implement test

---
+++ test/OpenJijDotNet.Tests/Utility/ClassicalScheduleListTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenJijDotNet.Utilities;
+using Xunit;
+
+// ReSharper disable once CheckNamespace
+namespace OpenJijDotNet.Tests.Utilities
+{
+
+    public class ClassicalScheduleListTest : TestBase
+    {
+
+        [Fact]
+        public void Create()
+        {
+            var parameter = new ClassicalScheduleList(11);
+            this.DisposeAndCheckDisposedState(parameter);
+        }
+
+        [Fact]
+        public void GetTuple()
+        {
+            var parameter = new ClassicalScheduleList(13);
+            var tuple = parameter.GetTuple();
+            Assert.Equal(13, tuple);
+            this.DisposeAndCheckDisposedState(parameter);
+        }
+
+    }
+
+}
